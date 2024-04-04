e5c3fdbb685b7314e23d895e60fde650f7a59486
safiylm <121391404+safiylm@users.noreply.github.com>
2024-04-04 14:54:15 +0200

    display html page

---
+++ src/main/java/com/example/articles/IndexController.java
@@ -0,0 +1,17 @@
+package com.example.articles;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path= "/")
+
+public class IndexController {
+
+    public String index(){
+        return "index";
+    }
+}
