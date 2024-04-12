90b6cdbfc1da99e05b03a2e9d0dc51522604eb68
safiylm <121391404+safiylm@users.noreply.github.com>
2024-04-12 12:06:44 +0200

    Update ArticlesApplication.java

--- src/main/java/com/example/articles/ArticlesApplication.java
+++ src/main/java/com/example/articles/ArticlesApplication.java
@@ -1,17 +1,10 @@
 package com.example.articles;
 
-import com.example.articles.blogarticle.BlogArticle;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 @SpringBootApplication
-@EnableMongoRepositories
-
 public class ArticlesApplication {
 
 	public static void main(String[] args) {
@@ -20,5 +13,6 @@ public class ArticlesApplication {
 
 
 }
+
 
 // spring.data.mongodb.uri=mongodb+srv://safii:SafinazYilmaz@Cluster0.mongodb.net/trelloo_db
